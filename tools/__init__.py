56cd8276f54edb936f71c61eefda3a8208edc6c1
ldotmithu <ldotmithurshan222@gmail.com>
2025-07-14 22:39:25 +0530

    Initialize Tools

--- tools/__init__.py
+++ tools/__init__.py
@@ -4,9 +4,24 @@ from tools.hotel_info import GoogleHotelSearchTool,TavilyHotelSearchTool
 from tools.currency_info import CurrencyTools
 
 # --- Initialize Tools ---
-weather_tools = WeatherInfoTool().weather_tool_list
-google_attraction_tools = GooglePlaceSearchTool().Google_Search_tool_list
-tavily_attraction_tools = TavilyPlaceSearchTool().tavily_search_tool_list
-google_hotel_tools = GoogleHotelSearchTool().Google_Search_tool_list
-tavily_hotel_tools = TavilyHotelSearchTool().tavily_search_tool_list
-currency_tools = CurrencyTools().currency_tool_list
+
+
+class InitializeTools:
+    def __init__(self):
+        self.weather_tools = WeatherInfoTool().weather_tool_list
+        self.google_attraction_tools = GooglePlaceSearchTool().Google_Search_tool_list
+        self.tavily_attraction_tools = TavilyPlaceSearchTool().tavily_search_tool_list
+        self.google_hotel_tools = GoogleHotelSearchTool().Google_Search_tool_list
+        self.tavily_hotel_tools = TavilyHotelSearchTool().tavily_search_tool_list
+        self.currency_tools = CurrencyTools().currency_tool_list
+    
+    def alltools(self):
+        all_tools = (
+            self.weather_tools + 
+            self.google_attraction_tools + 
+            self.tavily_attraction_tools + 
+            self.google_hotel_tools + 
+            self.tavily_hotel_tools + 
+            self.currency_tools
+        )
+        return all_tools
